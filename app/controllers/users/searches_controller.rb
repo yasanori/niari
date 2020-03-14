bba19d95dca1c4157cab3b22eddd8287432121d9
nagao <38156084+yasanori@users.noreply.github.com>
2020-03-14 20:08:41 +0900

    都道府県で検索

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -1,7 +1,8 @@
 class Users::SearchesController < ApplicationController
 
 def index
-  @users = User.search(params[:keyword]).where(camera_id: 1).page(params[:page]).per(15)
+  @users = User.search(params[:keyword]).where(camera_id: 1).where(prefecture_id: params[:prefecture]).page(params[:page]).per(15)
+  @outher_users =  User.where(camera_id: 1).page(params[:page]).per(15)
   respond_to do |format|
     format.html
     format.json
