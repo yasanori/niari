689b73db57331992e57a2910209d652db80b6825
nagao <38156084+yasanori@users.noreply.github.com>
2020-03-15 15:27:37 +0900

    DMリスト、ユーザー名のみ表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,26 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @rooms = current_user.rooms
+    @users_rooms = []
+
+    if @rooms.length != 0
+      @rooms.each do |room|
+        @user_room = {}
+        entry_list = Entry.where(room_id: room.id)
+        entry_list.each do |entry_user|
+          if entry_user.user_id == current_user.id
+          else
+            @user_room[:user] = entry_user.user
+            @user_room[:room] = room
+            @users_rooms <<  @user_room
+          end
+        end
+      end
+    end
+  end
+
   def create
     @room = Room.create
     @entryCu = Entry.create(room_id: @room.id, user_id: current_user.id)
