6cb686028a40e6f7c15b9938069cae4ed936b461
nagao <38156084+yasanori@users.noreply.github.com>
2020-03-31 22:33:55 +0900

    unicorn.rb 編集

--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -29,35 +29,6 @@ $(document).on('turbolinks:load', function () {
   });
 });
 
-
-
-
-
-
-
-
-
-
-// $(document).on('turbolinks:load', function () {
-//   $(function () {
-
-//     function buildHTML(count) {
-//       var html = `<div class="preview-box" id="preview-box__${count}">
-//                     <div class="upper-box">
-//                       <img src="" alt="preview">
-//                     </div>
-//                     <div class="lower-box">
-//                       <div class="update-box">
-//                         <label class="edit_btn">編集</label>
-//                       </div>
-//                       <div class="delete-box" id="delete_btn_${count}">
-//                         <span>削除</span>
-//                       </div>
-//                     </div>
-//                   </div>`
-//       return html;
-//     }
-
 if (window.location.href.match(/\/items\/\d+\/edit/)) {
   var prevContent = $('.label-content').prev();
   $('.preview-box').each(function (index, box) {
@@ -71,73 +42,3 @@ if (window.location.href.match(/\/items\/\d+\/edit/)) {
     $('.label-content').hide();
   }
 }
-
-
-//     $(document).on('change', '.hidden-field', function () {
-//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-//       $('.label-box').attr({ id: `label-box--${id}`, for: `item_item_images_attributes_${id}_image` });
-//       var file = this.files[0];
-//       var reader = new FileReader();
-//       $('.hidden-btn').show();
-//       reader.readAsDataURL(file);
-//       reader.onload = function () {
-//         var image = this.result;
-//         if ($(`#preview-box__${id}`).length == 0) {
-//           var count = $('.preview-box').length;
-//           var html = buildHTML(id);
-//           var prevContent = $('.label-content').prev();
-//           $(prevContent).append(html);
-//         }
-//         $(`#preview-box__${id} img`).attr('src', `${image}`);
-//         var count = $('.preview-box').length;
-//         if (count == 5) {
-//           $('.label-content').hide();
-//         }
-
-//         if ($(`#item_item_images_attributes_${id}_destroy`)) {
-//           $(`#item_item_images_attributes_${id}__destroy`).prop('checked', false);
-//         }
-
-//         if (count < 5) {
-//           $('.label-box').attr({ id: `label-box--${count}`, for: `item_item_images_attributes_${count}_image` });
-//         }
-//       }
-//     });
-
-//     $(document).on('click', '.delete-box', function () {
-//       var count = $('.preview-box').length;
-//       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-//       $(`#preview-box__${id}`).remove();
-
-//       if (id == 0) {
-//         $('.hidden-btn').hide();
-//       }
-
-//       if ($(`#item_item_images_attributes_${id}__destroy`).length == 0) {
-//         $(`#item_item_images_attributes_${id}_image`).val("");
-//         var count = $('.preview-box').length;
-//         if (count == 4) {
-//           $('.label-content').show();
-//         }
-//         if (id < 5) {
-//           $('.label-box').attr({ id: `label-box--${id}`, for: `item_item_images_attributes_${id}_image` });
-//         }
-//       } else {
-//         $(`#item_item_images_attributes_${id}__destroy`).prop('checked', true);
-//         if (count == 4) {
-//           $('.label-content').show();
-//         }
-//         var count = $('.preview-box').length;
-//         if (count == 4) {
-//           $('.label-content').show();
-//         }
-//         if (id < 5) {
-//           $('.label-box').attr({ id: `label-box--${id}`, for: `item_item_images_attributes_${id}_image` });
-//         }
-//       }
-//     });
-//     if ($('.preview-box').length == 0) {
-//       $('.hidden-btn').hide();
-//     }
-//   });
-// });
