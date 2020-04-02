ac3b78a3640236905e850c2399745903aee6e8ef
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-02 18:23:52 +0900

    s3

--- Gemfile
+++ Gemfile
@@ -89,3 +89,4 @@ gem 'devise-bootstrap-views', '~> 1.0'
 gem 'erb2haml'
 gem "acts_as_follower", github: "tcocca/acts_as_follower"
 gem 'seed-fu'
+gem 'fog-aws'
--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -6,7 +6,7 @@ $(document).on('turbolinks:load', function () {
         `
         <div class="prev-content">
           <img src="${image}", alt="preview" class="prev-image">
-        <div>
+        </div>
         `
       return html;
     }
@@ -28,17 +28,3 @@ $(document).on('turbolinks:load', function () {
     });
   });
 });
-
-if (window.location.href.match(/\/items\/\d+\/edit/)) {
-  var prevContent = $('.label-content').prev();
-  $('.preview-box').each(function (index, box) {
-    $(box).attr('id', `preview-box__${index}`);
-  })
-  $('.delete-box').each(function (index, box) {
-    $(box).attr('id', `delete_btn_${index}`);
-  })
-  var count = $('.preview-box').length;
-  if (count == 5) {
-    $('.label-content').hide();
-  }
-}
