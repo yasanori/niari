c1e6f36b22aac297399f83485ab04d7d55193d4c
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-13 14:28:03 +0900

    factroybot-setting

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name                    {"tanaka"}
     email                   {"test@gmail.com"}
     password                {"12345678"}
-    password_confimation    {"12345678"}
+    password_confirmation    {"12345678"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,17 @@ RSpec.describe User, type: :model do
 
   describe '#create' do
     it "nameがない場合は登録できないこと" do
-     user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", )
+     user = build(:user, name: "")
      user.valid?
      expect(user.errors[:name]).to include("を入力してください")
     end
+
+    it "emailがない場合は登録ができないこと" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
   end
 
 end
