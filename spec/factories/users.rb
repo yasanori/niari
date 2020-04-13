b2197d29c611331ea4c7daeb10166e3663253539
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-13 09:51:48 +0900

    factry_bot set

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-
+    name                    {"tanaka"}
+    email                   {"test@gmail.com"}
+    password                {"12345678"}
+    password_confimation    {"12345678"}
   end
 end
