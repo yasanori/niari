dd4fc6fc1b4cf99cb1a74f8f0c1f32cca23fbe8c
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-13 09:51:06 +0900

    add_rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#create' do
+    it "nameがない場合は登録できないこと" do
+     user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", )
+     user.valid?
+     expect(user.errors[:name]).to include("を入力してください")
+    end
+  end
+
 end
