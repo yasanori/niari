4798024e6e65cf0356eb21b2a6596c4d1b004194
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-16 08:25:17 +0900

    ユーザーのインクリメンタルサーチ

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,40 +1,272 @@
 $(document).on('turbolinks:load', function () {
   $(function () {
-    var search_list = $(".camera_boxes");
+    var search_list = $(".user-contants");
     function appendUser(user) {
       if (user.user_sign_in.id !== user.id) {
-        if (user.image.url == "nofoto.jpg") {
+        if (user.image.url == "nofoto.jpg" && user.prefecture == null && user.profile == null) {
+          console.log(1);
           var html =
             `
-            <div class="camera_contents">
-              <a class="camera_link" href="/users/${user.id}">
-                <div class='camera_box'>
-                  <div class='mini-prf-image-box'>
-                    <img class="mini-prf-image" src="/assets/nofoto-281bffacc1ae6b6f24b2170d7a182622072296aa7f34ae7cc4fcd4cee8601a9c.jpg" />
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
                   </div>
-                  <p class='camera_name'>
-                    ${user.name}
-                  </p>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="/assets/nofoto-281bffacc1ae6b6f24b2170d7a182622072296aa7f34ae7cc4fcd4cee8601a9c.jpg" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class="user-name">
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                    </div>
+                  </div>
                 </div>
-              </a>
-            </div>
+              </div>
+            </a>
+        `
+          search_list.append(html);
+        } else if (user.image.url == "nofoto.jpg" && user.prefecture == null) {
+          console.log(2);
+          var html =
+            `
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
+                  </div>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="/assets/nofoto-281bffacc1ae6b6f24b2170d7a182622072296aa7f34ae7cc4fcd4cee8601a9c.jpg" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class='user-name'>
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                      <p class='user-text'>
+                        ${user.profile}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+        `
+          search_list.append(html);
+        } else if (user.image.url == "nofoto.jpg" && user.profile == null) {
+          console.log(3);
+          var html =
+            `
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
+                  </div>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="/assets/nofoto-281bffacc1ae6b6f24b2170d7a182622072296aa7f34ae7cc4fcd4cee8601a9c.jpg" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class='user-name'>
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                        <p class='user-prefecture'>
+                          ${user.prefecture.name}
+                        </p>
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+        `
+          search_list.append(html);
+        } else if (user.profile == null && user.prefecture == null) {
+          console.log(4);
+          var html =
+            `
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
+                  </div>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="${user.image.url}" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class='user-name'>
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+        `
+          search_list.append(html);
+        } else if (user.prefecture == null) {
+          console.log(5);
+          var html =
+            `
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
+                  </div>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="${user.image.url}" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class='user-name'>
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                      <p class='user-text'>
+                        ${user.profile}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+        `
+          search_list.append(html);
+        } else if (user.profile == null) {
+          console.log(6);
+          var html =
+            `
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
+                  </div>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="/assets/nofoto-281bffacc1ae6b6f24b2170d7a182622072296aa7f34ae7cc4fcd4cee8601a9c.jpg" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class='user-name'>
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                        <p class='user-prefecture'>
+                          ${user.prefecture.name}
+                        </p>
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+        `
+          search_list.append(html);
+        } else if (user.image.url == "nofoto.jpg") {
+          console.log(7);
+          var html =
+            `
+            <a class="user_link" href="/users/${user.id}">
+              <div class="user-wrapper">
+                <div class="user-container">
+                  <div class="user-container__image">
+
+                  </div>
+                  <div class="user-container__profile">
+                    <div class="user-container__profile__main">
+                      <div class="user-container__profile__main__image">
+                        <img class="mini-prf-image" src="/assets/nofoto-281bffacc1ae6b6f24b2170d7a182622072296aa7f34ae7cc4fcd4cee8601a9c.jpg" />
+                      </div>
+                      <div class="user-container__profile__main__name">
+                        <p class='user-name'>
+                          ${user.name}
+                        </p>
+                      </div>
+                      <div class="user-container__profile__main__prefecture">
+                        <p class='user-prefecture'>
+                          ${user.prefecture.name}
+                        </p>
+                      </div>
+                    </div>
+                    <div class="user-container__profile__text">
+                      <p class='user-text'>
+                        ${user.profile}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
         `
           search_list.append(html);
         } else {
+          console.log(8);
           var html =
             `
-              <div class="camera_contents">
-                <a class="camera_link" href="/users/${user.id}">
-                  <div class='camera_box'>
-                    <div class='mini-prf-image-box'>
-                      <img class="mini-prf-image" src="${user.image.url}" />
+              <a class="user_link" href="/users/${user.id}">
+                <div class="user-wrapper">
+                  <div class="user-container">
+                    <div class="user-container__image">
+
                     </div>
-                    <p class='camera_name'>
-                      ${user.name}
-                    </p>
+                    <div class="user-container__profile">
+                      <div class="user-container__profile__main">
+                        <div class="user-container__profile__main__image">
+                          <img class="mini-prf-image" src="${user.image.url}" />
+                        </div>
+                        <div class="user-container__profile__main__name">
+                          <p class='user-name'>
+                            ${user.name}
+                          </p>
+                        </div>
+                        <div class="user-container__profile__main__prefecture">
+                          <p class='user-prefecture'>
+                            ${user.prefecture}
+                          </p>
+                        </div>
+                      </div>
+                      <div class="user-container__profile__text">
+                        <p class='user-text'>
+                          ${user.profile}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </a>
-              </div>
+                </div>
+              </a>
             `
           search_list.append(html);
         }
--- app/views/users/searches/index.json.jbuilder
+++ app/views/users/searches/index.json.jbuilder
@@ -2,6 +2,8 @@ json.array! @users do |user|
   json.id user.id
   json.name user.name
   json.image user.image
+  json.prefecture user.prefecture
+  json.text user.profile
   json.camera_id user.camera_id
   json.user_sign_in current_user
 end
