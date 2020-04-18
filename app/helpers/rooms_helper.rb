90723fe3de4aaffc148efffcca82beeae03f9c1d
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-18 17:31:54 +0900

    配列から取り出して変数に入れていた記述を削除

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -2,9 +2,8 @@ module RoomsHelper
 
   def most_new_message_preview(room)
     message = room.messages.order(updated_at: :desc).limit(1)
-    message = message[0]
     if message.present?
-      tag.p "#{message.text}", class: "dm_list__content__link__box__message"
+      tag.p "#{message[0].text}", class: "dm_list__content__link__box__message"
     else
       tag.p "[ まだメッセージはありません ]", class: "dm_list__content__link__box__message"
     end
@@ -12,8 +11,7 @@ module RoomsHelper
 
   def opponent_user(room)
     entry = room.entries.where.not(user_id: current_user)
-    name = entry[0].user.name
-    tag.p "#{name}", class: "dm_list__content__link__box__name"
+    tag.p "#{entry[0].user.name}", class: "dm_list__content__link__box__name"
 
   end
 
