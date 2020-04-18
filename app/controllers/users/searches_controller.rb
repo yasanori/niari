5fa2ec3cee968c9421ad95f4c9800676eec7ac89
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-18 17:13:07 +0900

    検索画面の分離

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -1,8 +1,8 @@
 class Users::SearchesController < ApplicationController
 
 def index
-  @users = User.search(params[:keyword]).where(camera_id: 1).page(params[:page]).per(15)
-  @outher_users =  User.where(camera_id: 1).page(params[:page]).per(15)
+  @users = User.search(params[:keyword]).where(camera_id: 1).where.not(id: current_user).page(params[:page]).per(15)
+  @outher_users =  User.where(camera_id: 1).where.not(id: current_user).page(params[:page]).per(15)
   respond_to do |format|
     format.html
     format.json
@@ -15,10 +15,9 @@ end
 
 def prefecture_search
   if params[:prefecture].present?
-    @users = User.where(camera_id: 1).where(prefecture_id: params[:prefecture]).page(params[:page]).per(15)
-  else
-    @outher_users = User.where(camera_id: 1).page(params[:page]).per(15)
+    @users = User.where(camera_id: 1).where(prefecture_id: params[:prefecture]).where.not(id: current_user).page(params[:page]).per(15)
   end
+  @other_users = User.where(camera_id: 1).where.not(id: current_user).page(params[:page]).per(15)
 end
 
 end
