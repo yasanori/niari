5f47d26cda7c29fefcbdf114c8132c4c3d8783b2
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-24 18:34:29 +0900

    worksマイグレーションファイルの編集

--- db/migrate/20200424090757_create_photograph_works.rb
+++ db/migrate/20200424090757_create_photograph_works.rb
@@ -1,8 +1,16 @@
 class CreatePhotographWorks < ActiveRecord::Migration[5.2]
   def change
     create_table :photograph_works do |t|
-
+      t.references :photographer
+      t.references :requester
+      t.date :date, null: false
+      t.integer :prefecture
+      t.text :text, null: false
+      t.integer :progress, default: 1
       t.timestamps
     end
+    add_foreign_key :photograph_works, :users, column: :photographer_id
+    add_foreign_key :photograph_works, :users, column: :requester_id
+    add_foreign_key :photograph_works, :prefectures, column: :prefecture_id
   end
 end
